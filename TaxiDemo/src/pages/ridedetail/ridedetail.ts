7f31fda20a610d328769169043dffdf32491e7bc
Aditya Kotamraju <aditya.kotamraju@cognizant.com>
2017-12-06 18:50:13 +0530

    Driver call option for upcoming rides

--- TaxiDemo/src/pages/ridedetail/ridedetail.ts
+++ TaxiDemo/src/pages/ridedetail/ridedetail.ts
@@ -10,16 +10,22 @@ import {RideModel} from '../payment/ride.model';
 export class RidedetailPage {
 public ride : RideModel;
 public staticMap: string;
+public isUpcoming: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log('params',this.navParams)
     this.staticMap = this.navParams.get("map");
     this.ride = this.navParams.get("params");
+    this.isUpcoming = this.navParams.get("isUpcoming");
   }
 
   ionViewDidLoad() {
     console.log("ride", this.ride);
     console.log('ionViewDidLoad RidedetailPage');
+  }
+
+  callDriver(phoneNumber) {
+    window.location.href = "tel://" + phoneNumber;
   }
 
 }
